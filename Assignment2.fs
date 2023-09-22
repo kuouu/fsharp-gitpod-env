45847c7b240405b831ff9ad041cdfd532b327e0f
Kuo Yu-Tsung <kuouu.tw@gmail.com>
2023-09-22 02:16:40 +0000

    update A2

--- Assignment2.fs
+++ Assignment2.fs
@@ -21,21 +21,24 @@ module recursive  =
 
 module higherOrderFunc  = 
     let cardNumber: int list = [4;7;1;6;6;6;3;0;3;6;5;5;4;1;2;3];
-    let doubleDigit = [for i in 1..cardNumber.Length -> match i % 2 with | 1 -> cardNumber.[cardNumber.Length - i] | _ -> cardNumber.[cardNumber.Length - i]*2]
-    let sum = doubleDigit |> List.sum;
-    match sum % 10 with
-    | 0 -> printfn "Valid"
-    | _ -> printfn "Invalid"
+    let calculateDigit (cardNumber: int list) : string = 
+        cardNumber
+        |> List.rev
+        |> List.mapi (fun i x -> match i % 2 with | 0 -> x | _ -> x*2)
+        |> List.sum
+        |> fun x -> match x % 10 with | 0 -> "Valid" | _ -> "Invalid";
+    printfn "%s" (calculateDigit cardNumber);
 
 
 (*
     Reflection:
-    In this assignment, I would prefer recursive function over higher order function. Due to the assignment requirement, 
-    I have to use collections modules that only taught in the class to implement. Even it is shorter than the recursive 
-    function, I think recursive function is more readable and easier to understand in this situation.
+    In this assignment, we perform the same task using recursive function and higher order function.
+    In the recursive function, I create three functions to perform the task, and connect them together
+    to get the result. 
 
-    If there is no restriction to use collections modules, I would prefer higher order function. It would be more
-    concise and easier to understand. Only one mapi and reduce function can solve the problem. As for the recursive
-    function, it would be more suitable for the situation that the problem contain nested structure, like dealing with 
-    tree or graph structure.
+    On the other hand, I use List.rev, List.mapi, List.sum to perform the same task in the higher order
+    function. It is more concise and intuitive. So, I would prefer higher order function in this situation.
+
+    However, if the problem is more complex, like dealing with tree or graph structure, I would prefer
+    recursive function. It would be more suitable for the situation that the problem contain nested structure.
 *)
